6046ae9eeef41b65f4dee6ba99111c7215884b54
aniljoindevops <yerramaniianilkumar@gmail.com>
2024-06-16 20:29:04 +0530

    functions

--- cicd/main.tf
+++ cicd/main.tf
@@ -32,7 +32,7 @@ module "jenkins_agent" {
 resource "aws_key_pair" "tools" {
   key_name   = "tools"
   # you can paste the public key directly like this
-  #public_key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIL6ONJth+DzeXbU3oGATxjVmoRjPepdl7sBuPzzQT2Nc sivak@BOOK-I6CR3LQ85Q"
+  #public_key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMlFH0CJrsPnM21P7sCaiJ0R8jSCsaTm6jx7EaQBfxOu yerra@LAPTOP-CUS9THHT
   public_key = file("~/.ssh/tools.pub")
   # ~ means windows home directory
 }
